f71ca21ba2eab9b50a4596202388a0fa52e4f62c
remixan07 <remixan07@gmail.com>
2026-01-31 16:04:19 -0600

    Avance de Evidencia - Versión 3 - 29/01/26 - 9:20

--- POO_Avance_Evidencia.java
+++ POO_Avance_Evidencia.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 
 public class POO_Avance_Evidencia {
     public static void main(String[] args) {
-        
+
+        public class Library {
+            List <book> bookList;
+            List <user> userList;
+            List <loaned> loanedList;
+
+            public Library() {
+                book
+            }
+
+        }
+
     }
-    
 }
